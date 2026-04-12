af6cc95a85dc5daf74ada2c5c2483955163b6388
Shivani-Shanbhag02 <shivanishanbhag02@gmail.com>
2026-04-12 20:45:35 +0530

    Added animations

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import SplashScreen from "./src/screens/SplashScreen";
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
 import CustomerHome from "./src/screens/CustomerHome";
@@ -11,25 +12,37 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-  name="Login"
-  component={LoginScreen}
-  options={{ headerShown: false }}
-/>
+      <Stack.Navigator initialRouteName="Splash">
 
-<Stack.Screen
-  name="Register"
-  component={RegisterScreen}
-  options={{ headerShown: false }}
-/>
-        <Stack.Screen name="CustomerHome" component={CustomerHome} />
+        <Stack.Screen
+          name="Splash"
+          component={SplashScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="CustomerHome"
+          component={CustomerHome}
+        />
 
         <Stack.Screen
           name="StaffHome"
           component={StaffTabNavigator}
           options={{ headerShown: false }}
         />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
