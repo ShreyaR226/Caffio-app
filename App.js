cdc3a7187ac0c5609acc4f21df6687131ea9fd4b
Shivani-Shanbhag02 <shivanishanbhag02@gmail.com>
2026-04-14 19:53:18 +0530

    error resolved

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import SplashScreen from "./src/screens/SplashScreen";
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
-// import CustomerHome from "./src/screens/CustomerHome";
-import StaffHome from "./src/screens/StaffHome";
 import CustomerTabs from "./src/screens/CustomerTabs";
 import PaymentScreen from "./src/screens/PaymentScreen";
 import { CartProvider } from "./src/context/CartContext";
-import CustomerHome from "./src/screens/CustomerHome";
 import StaffTabNavigator from "./src/navigation/StaffTabNavigator";
 
 const Stack = createNativeStackNavigator();
@@ -18,11 +14,8 @@ export default function App() {
   return (
     <CartProvider>
     <NavigationContainer>
-      <Stack.Navigator>
-      
      
-     
-      
+    
       <Stack.Navigator initialRouteName="Splash">
 
         <Stack.Screen
@@ -55,7 +48,7 @@ export default function App() {
          
          </Stack.Navigator>
 
-      </Stack.Navigator>
+    
     </NavigationContainer>
     </CartProvider>
   );
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const API = axios.create({
-  baseURL: "http://192.168.0.102:5000/api"
+  baseURL: "http://10.233.168.211:5000/api"
 });
 
 // 🔥 ADD THIS
